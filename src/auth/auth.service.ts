b2bdeba448466a414af47a0c6c8a4e04da87ddae
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2025-06-18 13:57:44 -0300

    fix(auth-services): validate password now not return code status 401 when change the password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { LoginDto } from './dto/auth.dto';
 import { TenantsService } from 'src/tenants/tenants.service';
 import { genSalt, hash } from 'bcrypt';
@@ -167,12 +171,19 @@ export class AuthService {
   async validatePassword(
     user: { salt?: string; password?: string },
     password: string,
+    throwError: boolean = true,
   ) {
     const hashedPassword = await hash(password, user.salt!);
 
     if (user.password !== hashedPassword) {
-      throw new UnauthorizedException(
-        `The credentials are not valid, please try again.`,
+      if (throwError) {
+        throw new UnauthorizedException(
+          `The credentials are not valid, please try again.`,
+        );
+      }
+
+      throw new BadRequestException(
+        'The password provided is not valid, please try again.',
       );
     }
 
