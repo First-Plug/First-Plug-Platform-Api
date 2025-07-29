0d4e9821dfa09969f4d521d90d82526a782050b8
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2025-07-29 10:20:38 -0300

    fix(shipments): retool socket when update shipment

--- src/infra/event-bus/events.gateway.ts
+++ src/infra/event-bus/events.gateway.ts
@@ -20,15 +20,12 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
     const tenantId = client.handshake.query.tenantId as string;
     if (tenantId) {
       client.join(`tenant-${tenantId}`);
-      console.log(`Client joined tenant: ${tenantId}`);
     } else {
       client.disconnect();
     }
   }
 
-  handleDisconnect() {
-    console.log('Client disconnected');
-  }
+  handleDisconnect() {}
 
   notifyTenant(tenantId: string, event: string, payload: any) {
     this.server.to(`tenant-${tenantId}`).emit(event, payload);
