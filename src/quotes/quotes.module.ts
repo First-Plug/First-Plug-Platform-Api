a44d747311d1de037b7ef3faa21fcf152040f0b5
MercedesPoisson <mercedespoisson@yahoo.com>
2026-01-21 12:51:19 -0300

    handle data wipe destination option

--- src/quotes/quotes.module.ts
+++ src/quotes/quotes.module.ts
@@ -9,9 +9,10 @@ import { TenantConnectionService } from '../infra/db/tenant-connection.service';
 import { TenantsModule } from '../tenants/tenants.module';
 import { tenantModels } from '../infra/db/tenant-models-provider';
 import { TenantsMiddleware } from '../common/middlewares/tenants.middleware';
+import { WarehousesModule } from '../warehouses/warehouses.module';
 
 @Module({
-  imports: [SlackModule, HistoryModule, TenantsModule],
+  imports: [SlackModule, HistoryModule, TenantsModule, WarehousesModule],
   providers: [
     QuotesService,
     QuotesCoordinatorService,
